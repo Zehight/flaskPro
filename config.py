0d3b2100f211e3c8537c931b806aa4f810eab5b6
Zehight <1318141712@qq.com>
2023-09-29 13:02:46 +0800

    ok

--- config.py
+++ config.py
@@ -3,7 +3,22 @@ import os
 # 是否开启debug模式
 DEBUG = True
 
+cpy_dev_config={
+    'username':'root',
+    'password':'Foriinrnge10+-;',
+    'db_address':'localhost:3306'
+}
+
+home_dev_config={
+    'username':'root',
+    'password':'Foriinrange10+-;',
+    'db_address':'localhost:3306'
+}
+
+now_config = home_dev_config
+
+
 # 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'Mccbts_Liubin123')
-db_address = os.environ.get("MYSQL_ADDRESS", '192.168.51.24:3306')
+username = os.environ.get("MYSQL_USERNAME", now_config['username'])
+password = os.environ.get("MYSQL_PASSWORD", now_config['password'])
+db_address = os.environ.get("MYSQL_ADDRESS", now_config['db_address'])
--- run.py
+++ run.py
@@ -23,5 +23,5 @@ app.config.from_object('config')
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    # app.run(host=sys.argv[1], port=sys.argv[2])
-    app.run(host='localhost', port=5000)
+    app.run(host=sys.argv[1], port=sys.argv[2])
+    # app.run(host='localhost', port=5000)
