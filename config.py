06d1dace286043fc12b71a6be436e74107b94cd2
毛欣鑫 <1318141712@qq.com>
2023-10-09 14:04:26 +0800

    ok

--- config.py
+++ config.py
@@ -19,13 +19,21 @@ line_dev_config={
     'SecretKey':'543z8DNeprhPzMUCi2GKDEG94rW6Wisv'
 }
 
+config={
+    'username':'root',
+    'password':'RZ3WHtJ4',
+    'db_address':'sh-cdb-php1aito.sql.tencentcdb.com:63541',
+    'SecretId':'AKIDeDPTO9i7RitzgVJwYK1a0DIDze5d6Rlt',
+    'SecretKey':'543z8DNeprhPzMUCi2GKDEG94rW6Wisv'
+}
 
-now_config = line_dev_config
+
+now_config = config
 
 
 # 读取数据库环境变量
 username = os.environ.get("MYSQL_USERNAME", now_config['username'])
 password = os.environ.get("MYSQL_PASSWORD", now_config['password'])
 db_address = os.environ.get("MYSQL_ADDRESS", now_config['db_address'])
-SecretId = now_config['SecretId']
-SecretKey = now_config['SecretKey']
+SecretId = os.environ.get("secret_id", now_config['SecretId'])
+SecretKey = os.environ.get("secret_key", now_config['SecretKey'])
